5efc15a454bd783af8a2503622582af73fd0c4e4
Ashish <ashishteddy2@gmail.com>
2019-11-19 15:14:26 +0530

    sketchy works 1.1

--- lib/gamelogic.dart
+++ lib/gamelogic.dart
@@ -7,13 +7,8 @@ enum token {
 int computerScore = 0;
 int playerScore = 0;
 token currentPlayer = token.x;
-bool isEnd;
-
-void updateScore(token currentPlayer) {
-  isEnd
-      ? currentPlayer == token.x ? playerScore++ : computerScore++
-      : isEnd = false;
-}
+bool isEnd = false;
+bool scoreUpdated = false;
 
 List<List<token>> board = [
   [null, null, null],
@@ -46,6 +41,15 @@ bool legitMove(token t) {
 //  }
 //}
 
+void updateScore(token currentPlayer) {
+  if (scoreUpdated == false) {
+    scoreUpdated = true;
+    isEnd
+        ? currentPlayer == token.x ? playerScore++ : computerScore++
+        : isEnd = false;
+  }
+}
+
 void gameReset() {
   board = [
     [null, null, null],
@@ -61,6 +65,7 @@ void gameReset() {
   isEnd = true;
   playerScore= 0;
   computerScore = 0;
+  scoreUpdated = false;
 }
 
 bool fullBoard(List<List<token>> board) {
@@ -76,18 +81,6 @@ bool fullBoard(List<List<token>> board) {
 }
 
 bool winnerCheck(List<List<token>> board, token currentPlayer) {
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      if (board[i][0] == currentPlayer &&
-          board[i][1] == currentPlayer &&
-          board[i][2] == currentPlayer) {
-        colorBoard[i][0] = true;
-        colorBoard[i][1] = true;
-        colorBoard[i][2] = true;
-        return true;
-      }
-    }
-  }
 
   for (int i = 0; i < 3; i++) {
     if (board[i][0] == currentPlayer &&
@@ -96,7 +89,8 @@ bool winnerCheck(List<List<token>> board, token currentPlayer) {
       colorBoard[i][0] = true;
       colorBoard[i][1] = true;
       colorBoard[i][2] = true;
-      playerScore++;
+      updateScore(currentPlayer);
+
       return true;
     }
     if (board[0][i] == currentPlayer &&
@@ -105,60 +99,33 @@ bool winnerCheck(List<List<token>> board, token currentPlayer) {
       colorBoard[0][i] = true;
       colorBoard[1][i] = true;
       colorBoard[2][i] = true;
-      playerScore++;
-      return true;
-    }
-    if (board[i][0] == token.o &&
-        board[i][1] == token.o &&
-        board[i][2] == token.o) {
-      colorBoard[i][0] = true;
-      colorBoard[i][1] = true;
-      colorBoard[i][2] = true;
-      return true;
-    }
-    if (board[0][i] == token.o &&
-        board[1][i] == token.o &&
-        board[2][i] == token.o) {
-      colorBoard[0][i] = true;
-      colorBoard[1][i] = true;
-      colorBoard[2][i] = true;
+      updateScore(currentPlayer);
+
       return true;
     }
   }
-  if (board[0][0] == token.x &&
-      board[1][1] == token.x &&
-      board[2][2] == token.x) {
+  if (board[0][0] == currentPlayer &&
+      board[1][1] == currentPlayer &&
+      board[2][2] == currentPlayer) {
     colorBoard[0][0] = true;
     colorBoard[1][1] = true;
     colorBoard[2][2] = true;
+    updateScore(currentPlayer);
     return true;
   }
-  if (board[0][2] == token.x &&
-      board[1][1] == token.x &&
-      board[2][0] == token.x) {
+
+  if (board[0][2] == currentPlayer &&
+      board[1][1] == currentPlayer &&
+      board[2][0] == currentPlayer) {
     colorBoard[0][2] = true;
     colorBoard[1][1] = true;
     colorBoard[2][0] = true;
+    updateScore(currentPlayer);
+
     return true;
   }
-  if (board[0][0] == token.o &&
-      board[1][1] == token.o &&
-      board[2][2] == token.o) {
-    colorBoard[0][0] = true;
-    colorBoard[1][1] = true;
-    colorBoard[2][2] = true;
-    return true;
-  }
-  if (board[0][2] == token.o &&
-      board[1][1] == token.o &&
-      board[2][0] == token.o) {
-    colorBoard[0][2] = true;
-    colorBoard[1][1] = true;
-    colorBoard[2][0] = true;
-    return true;
-  } else {
-    return false;
-  }
+
+  else return false;
 }
 
 String getCurrentStatus(List<List<token>> board) {
